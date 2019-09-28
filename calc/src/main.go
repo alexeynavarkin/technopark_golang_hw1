eeae386be0028c57abffcad22e0a071bdfe5c5db
a.navarkin <a.navarkin@corp.mail.ru>
2019-09-28 23:05:41 +0300

    Main implemented.

--- calc/src/main.go
+++ calc/src/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I am main! :)")
+	reader := bufio.NewReader(os.Stdin)
+	expression, errReader := reader.ReadString('\n')
+	if errReader != nil && errReader != io.EOF {
+		fmt.Println(errReader)
+		os.Exit(-1)
+	}
+
+	result, errEval := EvalFromString(expression)
+	if errEval != nil {
+		fmt.Println(errEval)
+		os.Exit(-1)
+	}
+
+	fmt.Print(result)
 }
